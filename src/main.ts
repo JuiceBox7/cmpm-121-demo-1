cb405933fec63db5e9bb0a4942cbed639469db6c
JuiceBox7 <juiceinbox7@gmail.com>
2023-10-10 16:38:38 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,19 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+let counter = 0;
+const score: HTMLDivElement = document.createElement("div");
+score.innerHTML = `🌮's: ${counter}`;
+app.append(score);
+
 const increaseButton: HTMLButtonElement = document.createElement("button");
 increaseButton.innerHTML = `🌮`;
 
 increaseButton.addEventListener("click", function handleClick(event) {
   console.log("button was clicked");
   console.log(event);
+  counter++;
+  score.innerHTML = `🌮's: ${counter}`;
 });
+
 app.append(increaseButton);
